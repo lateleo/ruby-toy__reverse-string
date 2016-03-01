90023b6167c2ca6df7d235ed0429ca1cbc63bbd0
Art Burtch <artburtch@huskers.unl.edu>
2016-03-01 03:29:25 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,19 @@
 def reverse_characters(str)
+    chars_ary = str.chars
+    new_ary = []
+    chars_ary.each do |elem|
+        new_ary.unshift(elem)
+    end
+    
+   str = new_ary.join
 end
 
 def reverse_words(str)
+    words_ary = str.split
+    new_ary = []
+    words_ary.each do |elem|
+        new_ary.unshift(elem)
+    end
+    
+   str = new_ary.join(" ")
 end
